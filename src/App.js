eba2b15f890fec1c9bb22197a957b244276a2502
Ankita Patil <73190796+Ankita-Nandkumar-Patil@users.noreply.github.com>
2023-02-11 11:06:43 +0530

    adding metamask wallet

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Form from './components/Form';
+import Walletcard from './components/Walletcard';
 
 function App() {
   return (
     <div className="App">
+      <Walletcard/>
       <Form></Form>
     </div>
   );
